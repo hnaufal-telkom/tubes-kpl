e6bf890de53ecb0ff2a59ffd4e3aa1158e71ef68
M-fathirizky <fathirrizky636@gmail.com>
2025-05-08 16:35:29 +0700

    Update Konfigurasi user dan pengajuan Cuti

--- Configuration/AppConfig.cs
+++ Configuration/AppConfig.cs
@@ -6,30 +6,32 @@
     {
         //  Konfigurasi User 
 
-        /// Batas maksimal percobaan login
         public int MaxLoginAttempts { get; set; }
 
-        /// Jumlah hari sebelum password user harus diganti
         public int PasswordExpiryDays { get; set; }
 
-        /// Role default untuk user baru
         public string DefaultUserRole { get; set; } = "User";
 
-        //  Konfigurasi Pengajuan Cuti 
 
-        /// Maksimal cuti tahunan yang dimiliki user
         public int MaxCutiTahunan { get; set; }
 
-        /// Maksimal cuti per pengajuan
         public int MaxCutiPerPengajuan { get; set; }
 
-        /// Apakah cuti bersama diperbolehkan
         public bool AllowCutiBersama { get; set; }
 
-        /// Minimal hari pengajuan cuti sebelum tanggal cuti
         public int MinHariPengajuanSebelum { get; set; }
 
-        /// Constructor default
         public AppConfig() { }
+
+        public AppConfig(int MaxLoginAttempts, int PasswordExpiryDays, string DefaultUserRole, int MaxCutiTahunan, int MaxCutiPerPengajuan, bool AllowCutiBersama, int MinHariPengajuanSebelum)
+        {
+            this.MaxLoginAttempts = MaxLoginAttempts;
+            this.PasswordExpiryDays = PasswordExpiryDays;
+            this.DefaultUserRole = DefaultUserRole;
+            this.MaxCutiTahunan = MaxCutiTahunan;
+            this.MaxCutiPerPengajuan = MaxCutiPerPengajuan;
+            this.AllowCutiBersama = AllowCutiBersama;
+            this.MinHariPengajuanSebelum = MinHariPengajuanSebelum;
+        }
     }
 }
