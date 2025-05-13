de3d843002196044ac41fcd9845107ebf4f26c48
M-fathirizky <fathirrizky636@gmail.com>
2025-05-14 01:11:47 +0700

    api

---
+++ WebAPI/Service/LeaveService.cs
@@ -0,0 +1,24 @@
+﻿using MainLibrary;
+using System.Collections.Generic;
+
+namespace WebAPI.Service
+{
+    public class LeaveRequestService
+    {
+        private readonly ILeaveRequestRepository _leaveRepository;
+
+        public LeaveRequestService(ILeaveRequestRepository leaveRepository)
+        {
+            _leaveRepository = leaveRepository;
+        }
+
+        public IEnumerable<LeaveRequest> GetPendingLeaveRequests() =>
+            _leaveRepository.GetPendingLeaveRequests();
+
+        public LeaveRequest GetLeaveById(string id) =>
+            _leaveRepository.GetLeaveById(id);
+
+        public IEnumerable<LeaveRequest> GetByUserId(string userId) =>
+            _leaveRepository.GetByUserId(userId);
+    }
+}
